606e7750bfd0139c2e6696bb1cf067f102f54f1a
Ricardo Artur Staroski <ricardo.staroski@senior.com.br>
2016-09-22 21:10:56 -0300

    Preparado esqueleto para o problema 1726 - Friends

---
+++ 1726-friends/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * <a href="https://www.urionlinejudge.com.br/judge/en/problems/view/1726">1726 Friends</a>
+ * 
+ * @author ricardo.staroski
+ */
+public class Main {
+
+	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	private static final PrintStream out = System.out;
+
+	public static void main(String[] args) throws IOException {
+		// TODO
+	}
+}
