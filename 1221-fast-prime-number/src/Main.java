ef9873fbf977b27283ed3cb1d4b61235e72dacf0
Ricardo Artur Staroski <ricardo.staroski@senior.com.br>
2016-09-22 21:06:49 -0300

    Ajuste nas explicações dos comentários do problema 1221 - Fast Prime Number

--- 1221-fast-prime-number/src/Main.java
+++ 1221-fast-prime-number/src/Main.java
@@ -23,7 +23,8 @@ public class Main {
 	}
 
 	private static boolean isPrime(long n) {
-		// números menores que 2 não são primos
+		// números menores que 2 não são primos, pelo menos não para o URI Online Judge
+		// no mundo real, os números negativos são primos quando o seu oposto também é primo
 		if (n < 2) {
 			return false;
 		}
@@ -31,18 +32,16 @@ public class Main {
 		if (n == 2) {
 			return true;
 		}
-		// se número é maior que 2 e é par, ele não é primo
+		// se n é maior que 2 e é par, ele não é primo
 		if (n % 2 == 0) {
 			return false;
 		}
-		// para verificar se um número n é primo
-		// não é necessário testar todos os números de 3 à n
-		// basta testar de 3 até a raíz quadrada de n
-		// enquanto i * i for menor ou igual à n
-		// significa que a iteração não alcançou a raíz de n
+		// para verificar se n é primo não é necessário testar todos os números de 3 à n
+		// basta testar se ele é divisível por algum número de 3 até a raíz quadrada de n
+		// se até chegar à raíz ele não foi divisível por nenhum número, ele é primo
+		// enquanto i * i for menor ou igual à n, significa que a iteração não alcançou a raíz de n
 		for (long i = 3; i * i <= n; i += 2) {
-			// se n / 1 não tiver resto, significa que n é divisivel por i
-			// logo, não pode ser primo
+			// se n dividido por i não tiver resto, significa que n é divisivel por i, então não é primo
 			if (n % i == 0) {
 				return false;
 			}
