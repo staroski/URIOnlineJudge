919dd397f2f20a73c34ab3de01bbd936cd482b45
Ricardo Artur Staroski <ricardo.staroski@senior.com.br>
2016-09-26 08:29:24 -0300

    otimização do código do problema 1726 - Friends

--- 1726-friends/src/Main.java
+++ 1726-friends/src/Main.java
@@ -15,13 +15,6 @@ public class Main {
 	private static final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	private static final PrintStream out = System.out;
 
-	public static String evaluate(StringBuilder expression) {
-		// esta é a implementação de um parser recursivo descendente
-		// que resolve a gramática do problema proposto pela URI
-		// sua chamadas recursivas resolvem a precedência de operadores
-		return parseUnionOrDifference(expression);
-	}
-
 	public static void main(String[] args) throws IOException {
 		String line = null;
 		while ((line = in.readLine()) != null && !line.isEmpty()) {
@@ -30,6 +23,27 @@ public class Main {
 		}
 	}
 
+	public static String evaluate(StringBuilder expression) {
+		// esta é a implementação de um parser descendente recursivo
+		// que resolve a gramática do problema proposto pela URI Online Judge
+		// sua chamadas recursivas resolvem a precedência de operadores
+		return parseUnionOrDifference(expression);
+	}
+
+	private static final char INTERSECTION = '*';
+	private static final char UNION = '+';
+	private static final char DIFFERENCE = '-';
+
+	private static final char OPEN_SET = '{';
+	private static final char CLOSE_SET = '}';
+	private static final char FIRST_SET_SYMBOL = 'A';
+	private static final char LAST_SET_SYMBOL = 'Z';
+
+	private static final char OPEN_BLOCK = '(';
+	private static final char CLOSE_BLOCK = ')';
+	private static final String EMPTY_SET = "{}";
+
+	// calcula a diferença (-) de dois conjuntos
 	private static String computeDifference(String value1, String value2) {
 		Set<Character> set1 = stringToSet(value1);
 		Set<Character> set2 = stringToSet(value2);
@@ -37,6 +51,7 @@ public class Main {
 		return setToString(set1);
 	}
 
+	// calcula a intersecção (*) de dois conjuntos
 	private static String computeIntersection(String value1, String value2) {
 		Set<Character> set1 = stringToSet(value1);
 		Set<Character> set2 = stringToSet(value2);
@@ -44,6 +59,7 @@ public class Main {
 		return setToString(set1);
 	}
 
+	// calcula a união (+) de dois conjuntos
 	private static String computeUnion(String value1, String value2) {
 		Set<Character> set1 = stringToSet(value1);
 		Set<Character> set2 = stringToSet(value2);
@@ -51,62 +67,86 @@ public class Main {
 		return setToString(set1);
 	}
 
-	private static String parseBlocks(StringBuilder expression) {
+	// verifica se o símbolo informado é um elemento válido para um conjunto
+	private static boolean isSetSymbol(char symbol) {
+		return symbol == OPEN_SET || symbol == CLOSE_SET || symbol >= FIRST_SET_SYMBOL && symbol <= LAST_SET_SYMBOL;
+	}
+
+	// faz o parse de um bloco
+	private static String parseBlock(StringBuilder expression) {
 		char symbol = expression.charAt(0);
-		if (symbol != '(') {
+		if (symbol != OPEN_BLOCK) {
 			return "";
 		}
 		expression.deleteCharAt(0);
 		String result = parseUnionOrDifference(expression);
+		if (expression.length() < 1) {
+			throw syntaxError(String.format("Unclosed block! Expected '%c'", CLOSE_SET));
+		}
 		symbol = expression.charAt(0);
-		if (symbol != ')') {
-			throw new RuntimeException("syntax error: unclosed block");
+		if (symbol != CLOSE_BLOCK) {
+			throw syntaxError(String.format("Unclosed block! Expected '%c' found '%c'", CLOSE_BLOCK, symbol));
 		}
 		expression.deleteCharAt(0);
 		return result.toString();
 	}
 
+	// faz o parse da operação '*'
 	private static String parseIntersection(StringBuilder expression) {
 		String value1 = parseSet(expression);
 		while (expression.length() > 0) {
-			char op = expression.charAt(0);
-			if (op != '*') {
+			char symbol = expression.charAt(0);
+			if (symbol != INTERSECTION) {
 				return value1;
 			}
 			expression.deleteCharAt(0);
 			String value2 = parseSet(expression);
+			if (value2.isEmpty()) {
+				throw syntaxError(String.format("Expected second operator for operation '%c'", symbol));
+			}
 			value1 = computeIntersection(value1, value2);
 		}
 		return value1;
 	}
 
+	// faz o parse de um conjunto
 	private static String parseSet(StringBuilder expression) {
-		String value = parseBlocks(expression);
+		String value = parseBlock(expression);
 		StringBuilder result = new StringBuilder(value);
 		if (expression.length() > 0) {
 			char symbol = expression.charAt(0);
-			if (symbol != '{') {
+			if (symbol != OPEN_SET) {
 				return value;
 			}
 			do {
+				if (expression.length() < 1) {
+					throw syntaxError(String.format("Unclosed set! Expected '%c'", CLOSE_SET));
+				}
 				symbol = expression.charAt(0);
+				if (!isSetSymbol(symbol)) {
+					throw syntaxError(String.format("Invalid symbol! Expected '[%c-%c]' found '%c'", FIRST_SET_SYMBOL, LAST_SET_SYMBOL, symbol));
+				}
 				result.append(symbol);
 				expression.deleteCharAt(0);
-			} while (symbol != '}');
+			} while (symbol != CLOSE_SET);
 		}
 		return result.toString();
 	}
 
+	// faz o parse das operações '+' e '-'
 	private static String parseUnionOrDifference(StringBuilder expression) {
 		String value1 = parseIntersection(expression);
 		while (expression.length() > 0) {
-			char op = expression.charAt(0);
-			if (op != '-' && op != '+') {
+			char symbol = expression.charAt(0);
+			if (symbol != UNION && symbol != DIFFERENCE) {
 				return value1;
 			}
 			expression.deleteCharAt(0);
 			String value2 = parseIntersection(expression);
-			if (op == '+') {
+			if (value2.isEmpty()) {
+				throw syntaxError(String.format("Expected second operator for operation '%c'", symbol));
+			}
+			if (symbol == UNION) {
 				value1 = computeUnion(value1, value2);
 			} else {
 				value1 = computeDifference(value1, value2);
@@ -115,25 +155,30 @@ public class Main {
 		return value1;
 	}
 
+	// transforma um Set<Character> em uma String
 	private static String setToString(Set<Character> set) {
-		StringBuilder text = new StringBuilder("{");
-		for (Character c : set) {
-			text.append(c);
+		StringBuilder text = new StringBuilder().append(OPEN_SET);
+		for (Character symbol : set) {
+			text.append(symbol);
 		}
-		text.append("}");
+		text.append(CLOSE_SET);
 		return text.toString();
 	}
 
+	// transforma uma String em um Set<Character>
 	private static Set<Character> stringToSet(String value) {
 		TreeSet<Character> set = new TreeSet<>();
-		if (!"{}".equals(value)) {
+		if (!EMPTY_SET.equals(value)) {
 			value = value.substring(1, value.length() - 1); // remover { e }
-			// se o URI rodasse em Java 8, poderia fazer assim:
-			// set = value.chars().mapToObj(e -> (char) e).collect(Collectors.toSet());
 			for (char symbol : value.toCharArray()) {
 				set.add(symbol);
 			}
 		}
 		return set;
+	}
+
+	// utilizado para lançar erros de sintaxe
+	private static RuntimeException syntaxError(String message) {
+		return new RuntimeException("syntax error: " + message);
 	}
 }
